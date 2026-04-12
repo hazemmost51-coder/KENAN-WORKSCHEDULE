69c351a2dbf3ff5a5fbe5214d2b50ebb984c3e54
hazemmost51-coder <hazemmost51@gmail.com>
2026-04-12 21:54:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -308,8 +308,8 @@ function exportToExcel() {
     const headers = headerLabels.map(h => ({ v: h, s: headerStyle }));
 
     // مصفوفات لتخزين بيانات الشيت الأول والثاني
-    let dataWithConsultant = [[{ v: `${dateStr} - جدول الاعمال اليومي شركة كنان - بدون استشاري - مهندس السلامة: محمد مشرقي (0) - `, s: titleStyle }], headers];
-    let dataNoConsultant = [[{ v: `${dateStr} - جدول الاعمال اليومي شركة كنان - استشاري - مهندس السلامة: محمد مشرقي (0) - `, s: titleStyle }], headers];
+    let dataWithConsultant = [[{ v: `${dateStr} - جدول الاعمال اليومي شركة كنان - استشاري - مهندس السلامة: محمد مشرقي (+966 50 046 9088 ) - `, s: titleStyle }], headers];
+    let dataNoConsultant = [[{ v: `${dateStr} - جدول الاعمال اليومي شركة كنان -بدون استشاري - مهندس السلامة: محمد مشرقي (+966 50 046 9088) - `, s: titleStyle }], headers];
 
     // --- 2. توزيع البيانات بناءً على الاستشاري ---
     Object.entries(allWorkOrders).forEach(([no, d]) => {
